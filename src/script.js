c94e6ce20f8dd9f8e8891ba19aa1bc6370cfe9de
evegithu-hue <evegithu@gmail.com>
2026-05-19 08:58:45 +0200

    Updated weather conditions with real time data

--- src/script.js
+++ src/script.js
@@ -4,6 +4,12 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
 }
 
 function searchCity(city) {
@@ -19,3 +25,22 @@ function handleSearchSubmit(event) {
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function formatDate(formatDate) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[formatDate.getDay()];
+  let hours = formatDate.getHours();
+  let minutes = formatDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
